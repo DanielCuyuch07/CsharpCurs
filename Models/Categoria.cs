d50042d8a7fa87ef760745f9e9d24c3823730eac
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-11-11 17:31:22 -0600

    [UPDATE] Creando modulo de tarea con Fluent API

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -5,14 +5,11 @@ namespace proyecto.Models;
 
 public class Categoria{
 
-    [Key]
     public Guid CategoriaId {get; set;}
-    [Required]
-    [MaxLength(150)]
+    // [Required]
+    // [MaxLength(150)]
     public string Nombre {get; set;}
-    [Required]
-    
+
     public string Descripcion { get; set; }
-    [Required]
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
--- TareasContext.cs
+++ TareasContext.cs
@@ -7,4 +7,19 @@ public class TareasContext: DbContext{
     public DbSet<Tarea> Tareas { get; set; }
     public TareasContext(DbContextOptions<TareasContext> options): base(options){ }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        // Configuraciones especificas en el modelo de datos 
+        modelBuilder.Entity<Categoria>(Categoria =>
+        {
+            // EL Nombre de la tabla de la base de datos 
+            Categoria.ToTable("Categoria");
+            // Configuracion de la llave primaria
+            Categoria.HasKey(p => p.CategoriaId);
+            // Configuracion de las propiedades 
+            Categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
+
+            Categoria.Property(p => p.Descripcion);
+        });
+    }
 }
+
