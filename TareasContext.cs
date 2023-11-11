f0d1ee51fa6a62e106cc4fa690c0f8d139dec3bb
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-11-10 21:01:41 -0600

    [UPDATE] Mapeo de modulos usando atributos

--- TareasContext.cs
+++ TareasContext.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 public class TareasContext: DbContext{
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Tarea> Tareas { get; set; }
-
     public TareasContext(DbContextOptions<TareasContext> options): base(options){ }
 
 }
