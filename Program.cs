cab795c43ec1f7c487f05355ce9fd36143b32337
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-11-11 16:45:22 -0600

    [UPDATE] Conexion entre SQL Server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyecto.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+/* 
+
+    Configurar Entity Framework antes de definir rutas
+    Util : Base  de datos en memoria 
+ */
+
+
+// builder.Services.AddDbContext<TareasContext>(options =>
+// {
+//     options.UseInMemoryDatabase("TareasDB");
+// });
+
+builder.Services.AddSqlServer<TareasContext>("Data Source=(local); Initial Catalog= TareasDb;  Integrated Security=True; TrustServerCertificate=True");
+
+
+
 var app = builder.Build();
 
+/* Mapeo de rutas */
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async (HttpContext context, [FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
