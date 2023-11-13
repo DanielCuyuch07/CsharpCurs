8d286ed8dcc7792285ce2db4eff7ddcf5c32bab9
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-11-13 16:44:45 -0600

    [UPDATE] Finald

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyecto.Models;
@@ -27,6 +28,7 @@ var app = builder.Build();
 
 /*************************  Mapeo de enrutamientos ****************************/
 
+/*Get*/
 app.MapGet("/dbconexion", (HttpContext context, [FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
@@ -49,6 +51,41 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
 
     await dbContext.SaveChangesAsync();
     return Results.Ok();
+});
+
+/*UPDATE*/
+app.MapPut("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+
+    if (tareaActual != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        // Confirmar los datos 
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+    return Results.NotFound();
+});
+
+/*Delete*/
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+
+    // Buscar el registro actual 
+    var tareaActual = dbContext.Tareas.Find(id);
+    if (tareaActual != null)
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+    return Results.NotFound();
 });
 
 
